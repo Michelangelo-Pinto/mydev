b628eca4b8f6bbd4eac250074f69faf91b60ba47
Michelangelo Pinto <michelangelo.pinto93@gmail.com>
2025-06-09 02:09:59 +0200

    going on

---
+++ .devcontainer/container/libexec/utils.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# ==============================================================================
+# File:         utils.sh
+# Description:  Common reusable Bash functions for devops scripts.
+# Author:       Michelangelo Pinto
+# ==============================================================================
+
+check_required_vars() {
+  local context=$1
+  shift
+  local missing=false
+
+  if [[ $# -lt 2 ]]; then
+      echo "Usage: check_required_vars <context> <message> [<message> ...]"
+      exit 1
+  fi
+
+  for var in "$@"; do
+    if [[ -z "${!var:-}" ]]; then
+      log_warning "[$context] The variable $var is not set."
+      missing=true
+    fi
+  done
+
+  $missing && return 1 || return 0
+}
